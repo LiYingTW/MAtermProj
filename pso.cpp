de692a33988bff636c8b9e1f9d2d6814d460bac7
Li-Yng <ly101u@cs.ccu.edu.tw>
2016-06-18 15:04:42 +0800

    init

---
+++ pso.cpp
@@ -0,0 +1,17 @@
+#include "pso.h"
+
+
+PSO::PSO()
+{
+	return;
+}
+
+PSO::~PSO()
+{
+	return;
+}
+
+void PSO::PSOSlove()
+{
+	return;
+}
---
+++ pso.h
@@ -0,0 +1,14 @@
+#ifndef PSO_H
+#define PSO_H
+
+class PSO
+{
+public:
+	PSO();
+	~PSO();
+
+	void PSOSlove();
+private:
+};//end class PSO
+
+#endif
